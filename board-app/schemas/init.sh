53c684e11ae2cd9def7c14c16e147388b86c5419
nju33 <jun.sasaki@mvrck.co.jp>
2016-05-23 06:58:15 +0900

    .

--- board-app/schemas/init.sh
+++ board-app/schemas/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+if [ -n $1 ]; then
+  PASS="root"
+fi
 DB_NAME="tmp_board"
 
-mysql -uroot < db.sql
-mysql -uroot $DB_NAME < thread.sql
-mysql -uroot $DB_NAME < res.sql
+mysql -uroot -p${PASS} < db.sql
+mysql -uroot -p${PASS} $DB_NAME < thread.sql
+mysql -uroot -p${PASS} $DB_NAME < res.sql
